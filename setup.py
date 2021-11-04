2dd9f58a2c23588e2ba5311b0245a6b389d0081b
Peter Evans <peter.evans@cerberusnuclear.com>
2021-11-04 10:00:58 +0000

    Added test workflows

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Cerberus-Nuclear/Eddy-Wrapper",
-    install_requires=['Jinja2', 'importlib-resources', 'eddy-mc-core'],
+    install_requires=['importlib-resources', 'eddy-mc-core'],
     classifiers=[
         "Programming Language :: Python :: 3",
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
